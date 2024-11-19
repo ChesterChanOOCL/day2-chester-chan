594d2f1efcbfcb8c542484aca8a9f90533e1268c
CHANCH10 <chester.chan@oocl.com>
2024-11-19 10:37:31 +0800

    test: write test case for number 0

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -19,4 +19,19 @@ class MultiplicationTableBuilderTest {
     }
 
     // TODO: add more tests for isInRange
+    @Test
+    void should_return_true_when_isInRange_given_a_number_0() {
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 0;
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+        assertTrue(isInRange);
+    }
+
+    @Test
+    void should_return_true_when_isInRange_given_a_number_1001() {
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 1001;
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+        assertTrue(isInRange);
+    }
 }
