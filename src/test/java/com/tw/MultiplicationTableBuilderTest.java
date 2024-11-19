3fd1ea940479689ccada5e78d8039e2cb8ae4250
CHANCH10 <chester.chan@oocl.com>
2024-11-19 11:01:59 +0800

    test: make the should_return_true_when_isStartNotBiggerThanEnd_given_start_2_end_3()

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -27,11 +27,29 @@ class MultiplicationTableBuilderTest {
         assertTrue(isInRange);
     }
 
+
     @Test
     void should_return_true_when_isInRange_given_a_number_1001() {
         MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
         int number = 1001;
         boolean isInRange = multiplicationTableBuilder.isInRange(number);
         assertTrue(isInRange);
+    }
+    @Test
+    void should_return_true_when_isStartNotBiggerThanEnd_given_start_2_end_3(){
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 2;
+        int end = 3;
+        boolean  starterNotBiggerThanEnd = multiplicationTableBuilder.isStarterNotBiggerThanEnd(2,3);
+        assertTrue(starterNotBiggerThanEnd);
+    }
+
+    @Test
+    void should_return_true_when_isStartNotBiggerThanEnd_given_start_3_end_3(){
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 3;
+        int end = 3;
+        boolean  starterNotBiggerThanEnd = multiplicationTableBuilder.isStarterNotBiggerThanEnd(3,3);
+        assertTrue(starterNotBiggerThanEnd);
     }
 }
